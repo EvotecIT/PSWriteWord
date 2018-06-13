62a96015807b97a844746b223f7d080d7770b2c7
PrzemyslawKlys <pklys@evotec.pl>
2018-06-13 18:30:07 +0200

    Updated main file

--- PSWriteWord.psm1
+++ PSWriteWord.psm1
@@ -1,6 +1,7 @@
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$Assembly = @( Get-ChildItem -Path $PSScriptRoot\Lib\*.dll -ErrorAction SilentlyContinue )
 
 #Dot source the files
 Foreach ($import in @($Public + $Private)) {
@@ -10,9 +11,12 @@ Foreach ($import in @($Public + $Private)) {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
+Foreach ($import in @($Assembly)) {
+    Try {
+        Add-Type -Path $import.fullname
+    } Catch {
+        Write-Error -Message "Failed to import DLL $($import.fullname): $_"
+    }
+}
 
-$AssemblyName = "$($PSScriptRoot)\Lib\Xceed.Words.NET.dll"
-Add-Type -Path $AssemblyName
-#Export-ModuleMember -Function $Public.Basename -Verbose
-#Export-ModuleMember -Function * #-Verbose # 'Start-ADReporting', 'Get-KerberosLogonEvents', 'Get-GroupPolicyChanges', 'Get-EventLogClearedLogs'
 Export-ModuleMember -Function '*'
