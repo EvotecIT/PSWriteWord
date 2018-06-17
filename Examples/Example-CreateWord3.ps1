f7b019c7eaae4a44054d4663e76d69711ee249a2
PrzemyslawKlys <pklys@evotec.pl>
2018-06-17 15:39:13 +0200

    Example

--- Examples/Example-CreateWord3.ps1
+++ Examples/Example-CreateWord3.ps1
@@ -1,10 +1,10 @@
-Import-Module PSWriteWord -Force
+Import-Module PSWriteWord #-Force
 
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-CreateWord3.docx"
 
 $WordDocument = New-WordDocument $FilePath
-#$WordDocument.InsertParagraph("This is a text").FontSize("10").FontColor() | Out-Null
-#$WordDocument.InsertParagraph("Like me like i do").FontSize("21") | Out-Null
-#$WordDocument.InsertParagraph("Process").FontSize("15") | Out-Null
-Add-WordText -WordDocument $WordDocument -Text 'This is text that has font size of 15', ' and this is font size of 10 ', ' while this will be 12.' -FontSize 15, 10, 12 -Color White, Yellow, White
+$WordDocument.InsertParagraph("This is a text").FontSize("10") | Out-Null
+$WordDocument.InsertParagraph("Like me like i do").FontSize("21") | Out-Null
+$p = $WordDocument.InsertParagraph("Process").FontSize("15")
+$p.StyleName = [HeadingType]::Heading3
 Save-WordDocument $WordDocument
