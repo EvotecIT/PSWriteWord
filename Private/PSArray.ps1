7b643ff57e0480db33318f3398c505962151e3bb
PrzemyslawKlys <pklys@evotec.pl>
2018-06-17 21:53:33 +0200

    Supportive function

---
+++ Private/PSArray.ps1
@@ -0,0 +1,37 @@
+function New-ArrayList {
+    [CmdletBinding()]
+    param(
+
+    )
+    $List = New-Object System.Collections.ArrayList
+    <#
+    Mathias Rørbo Jessen:
+        The pipeline will attempt to unravel the list on assignment,
+        so you'll have to either wrap the empty arraylist in an array,
+        like above, or call WriteObject explicitly and tell it not to, like so:
+        $PSCmdlet.WriteObject($List,$false)
+    #>
+    return , $List
+}
+function Add-ToArray {
+    [CmdletBinding()]
+    param(
+        $List,
+        [ValidateNotNullOrEmpty()][Object] $Element
+    )
+    Write-Verbose $Element
+    $List.Add($Element) > $null
+}
+function Remove-FromArray {
+    param(
+        [System.Collections.ArrayList] $List,
+        [Object] $Element,
+        [switch] $LastElement
+    )
+    if ($LastElement) {
+        $LastID = $List.Count - 1
+        $List.RemoveAt($LastID) > $null
+    } else {
+        $List.Remove($Element) > $null
+    }
+}
