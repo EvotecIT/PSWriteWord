7d7345f82f47251a4620ee94447dd2fb10217042
PrzemyslawKlys <pklys@evotec.pl>
2018-06-17 13:31:16 +0200

    Updated example

--- Enums/Alignment.ps1
+++ Enums/Alignment.ps1
@@ -1,30 +1,6 @@
-<#
-/// <summary>
-/// Text alignment of a Paragraph.
-/// </summary>
-#>
 enum Alignment {
-    <#
-    /// <summary>
-    /// Align Paragraph to the left.
-    /// </summary>
     left
-    <#
-    /// <summary>
-    /// Align Paragraph as centered.
-    /// </summary>
-    #>
     center
-    <#
-    /// <summary>
-    /// Align Paragraph to the right.
-    /// </summary>
-    #>
     right
-    <#
-    /// <summary>
-    /// (Justified) Align Paragraph to both the left and right margins adding extra space between content as necessary.
-    /// </summary>
-    #>
     both
 }
--- Examples/Example-CreateWord2.ps1
+++ Examples/Example-CreateWord2.ps1
@@ -1,9 +1,18 @@
-Import-Module PSWriteWord -Force
+Import-Module PSWriteWord #-Force
 
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-CreateWord2.docx"
 
 $WordDocument = New-WordDocument $FilePath
-$WordDocument.InsertParagraph("This is a text").FontSize("10").FontColor() | Out-Null
-$WordDocument.InsertParagraph("Like me like i do").FontSize("21") | Out-Null
-$WordDocument.InsertParagraph("Process").FontSize("15") | Out-Null
+$p1 = $WordDocument.InsertParagraph("This is a text").FontSize("10").SpacingAfter(50)
+$p1.Alignment = 'center'
+
+
+$p2 = $WordDocument.InsertParagraph("Like me like i do").FontSize("21")
+$p2.Alignment = 'left'
+$p2.Direction = [Direction]::RightToLeft # 'RightToLeft'
+
+$p3 = $WordDocument.InsertParagraph("Process").FontSize("15")
+$p3.Direction = [Direction]::RightToLeft
+$p3.Alignment = 'both'
+
 Save-WordDocument $WordDocument
