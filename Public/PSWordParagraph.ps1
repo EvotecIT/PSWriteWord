0ccfa3aea5fe0c54d534af2f0760b2389dcb772d
PrzemyslawKlys <pklys@evotec.pl>
2018-06-17 15:39:54 +0200

    function update

--- Public/PSWordParagraph.ps1
+++ Public/PSWordParagraph.ps1
@@ -8,102 +8,63 @@
 # Paragraph InsertParagraph( string text, bool trackChanges, Formatting formatting )
 
 function Add-WordText {
-    <#
-	.SYNOPSIS
-	Write-Color is a wrapper around Write-Host.
-	It provides:
-	- easy manipulation of colors,
-	- logging output to file (log)
-	- nice formatting options out of the box.
-	.DESCRIPTION
-	Author: przemyslaw.klys at evotec.pl
-	Project website: https://evotec.xyz/hub/scripts/write-color-ps1/
-	Project support: https://github.com/EvotecIT/PSWriteColor
 
-	Original idea: Josh (https://stackoverflow.com/users/81769/josh)
-
-    version 0.5 (25th April 2018
-    - added backgroundcolor
-    - added aliases T/B/C to shorter code
-    - added alias to function (can be used with "WC")
-	- fixes to module publishing
-
-	version 0.4.0-0.4.9 (25th April 2018)
-	- published as module
-	- fixed small issues
-
-	version 0.31 (20th April 2018)
-	- Added Try/Catch for Write-Output (might need some additional work)
-	- Small change to parameters
-
-	version 0.3 (9th April 2018)
-	- added -ShowTime
-	- added -NoNewLine
-	- added function description
-	- changed some formatting
-
-	version 0.2
-	- added logging to file
-
-	version 0.1
-	- first draft
-
-	Notes:
-	- TimeFormat https://msdn.microsoft.com/en-us/library/8kb3ddd4.aspx
-
-	.EXAMPLE
-	Write-Color -Text "Red ", "Green ", "Yellow " -Color Red,Green,Yellow
-
-	Write-Color -Text "This is text in Green ",
-					"followed by red ",
-					"and then we have Magenta... ",
-					"isn't it fun? ",
-					"Here goes DarkCyan" -Color Green,Red,Magenta,White,DarkCyan
-
-	Write-Color -Text "This is text in Green ",
-					"followed by red ",
-					"and then we have Magenta... ",
-					"isn't it fun? ",
-					"Here goes DarkCyan" -Color Green,Red,Magenta,White,DarkCyan -StartTab 3 -LinesBefore 1 -LinesAfter 1
-
-	Write-Color "1. ", "Option 1" -Color Yellow, Green
-	Write-Color "2. ", "Option 2" -Color Yellow, Green
-	Write-Color "3. ", "Option 3" -Color Yellow, Green
-	Write-Color "4. ", "Option 4" -Color Yellow, Green
-	Write-Color "9. ", "Press 9 to exit" -Color Yellow, Gray -LinesBefore 1
-
-	Write-Color -LinesBefore 2 -Text "This little ","message is ", "written to log ", "file as well." `
-				-Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt" -TimeFormat "yyyy-MM-dd HH:mm:ss"
-	Write-Color -Text "This can get ","handy if ", "want to display things, and log actions to file ", "at the same time." `
-				-Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt"
-
-
-    # Added in 0.5
-    Write-Color -T "My text", " is ", "all colorful" -C Yellow, Red, Green -B Green, Green, Yellow
-    wc -t "my text" -c yellow -b green
-    wc -text "my text" -c red
-
-    #>
-    [Alias("wc")]
     param (
-        $WordDocument,
+        [Xceed.Words.NET.Container]$WordDocument,
         [alias ("T")] [String[]]$Text,
-        [alias ("C")] [ConsoleColor[]]$Color = "White",
-        [alias ("B")] [ConsoleColor[]]$BackGroundColor = $null,
-        [alias ("S")] [double[]] $FontSize = 10,
-        [int] $StartTab = 0,
-        [int] $LinesBefore = 0,
-        [int] $LinesAfter = 0,
-        [alias ("L")] [string] $LogFile = "",
+        [alias ("C")] [System.Drawing.Color[]]$Color = @(),
+        [alias ("S")] [double[]] $FontSize = @(),
+        [alias ("N")] [string[]] $FontName = @(),
+        [alias ("B")] [bool[]] $Bold = @(),
+        [alias ("I")] [bool[]] $Italic = @(),
+        [alias ("U")] [UnderlineStyle[]] $UnderlineStyle = @(),
+        [alias ("SA")] [double[]] $SpacingAfter = @(),
+        [alias ("SB")] [double[]] $SpacingBefore = @(),
+        [alias ("SP")] [double[]] $Spacing = @(),
+        [alias ("H")] [highlight[]] $Highlight = @(),
+        [alias ("CA")] [CapsStyle[]] $CapsStyle = @(),
+        [alias ("ST")] [StrikeThrough[]] $StrikeThrough = @(),
+        [alias ("HT")] [HeadingType[]] $HeadingType = @(),
+        #  [int] $StartTab = 0,
+        #  [int] $LinesBefore = 0,
+        #  [int] $LinesAfter = 0,
+        #  [alias ("L")] [string] $LogFile = "",
         [string] $TimeFormat = "yyyy-MM-dd HH:mm:ss",
         [switch] $ShowTime,
-        [switch] $NoNewLine
+        [switch] $NoNewLine,
+        [switch] $KeepLinesTogether,
+        [switch] $KeepWithNextParagraph
     )
+    #$WordDocument.GetType()
+    $p = $WordDocument.InsertParagraph()
+    for ($i = 0; $i -lt $Text.Length; $i++) {
+        # Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine
+        $p = $p.Append($Text[$i])
 
-    $DefaultColor = $Color[0]
+        if ($Color[$i] -ne $null) {
+            $p = $p.Color($Color[$i])
+        }
+        if ($FontSize[$i] -ne $null) {
+            $p = $p.FontSize($FontSize[$i])
+        }
+        if ($FontName[$i] -ne $null) {
+            $p = $p.FontName($FontSize[$i])
+        }
+        if ($Bold[$i] -ne $null -and $Bold[$i] -eq $true) {
+            $p = $p.Bold()
+        }
+        if ($Italic[$i] -ne $null -and $Italic[$i] -eq $true) {
+            $p = $p.Italic()
+        }
+
+
+    }
+
+
+    #$DefaultColor = $Color[0]
     #if ($BackGroundColor -ne $null -and $BackGroundColor.Count -ne $Color.Count) { Write-Error "Colors, BackGroundColors parameters count doesn't match. Terminated." ; return }
     if ($Text.Count -eq 0) { return }
-
+    return
     #if ($LinesBefore -ne 0) {  for ($i = 0; $i -lt $LinesBefore; $i++) { Write-Host "`n" -NoNewline } } # Add empty line before
     #if ($ShowTime) { Write-Host "[$([datetime]::Now.ToString($TimeFormat))]" -NoNewline} # Add Time before output
     #if ($StartTab -ne 0) {  for ($i = 0; $i -lt $StartTab; $i++) { Write-Host "`t" -NoNewLine } }  # Add TABS before text
@@ -111,10 +72,10 @@ function Add-WordText {
     if ($Color.Count -ge $Text.Count) {
         # the real deal coloring
         if ($BackGroundColor -eq $null) {
-            $p = $WordDocument.InsertParagraph($Text[0]).FontSize($FontSize[0]) #| Out-Null
-            for ($i = 1; $i -lt $Text.Length; $i++) {
+            $p = $WordDocument.InsertParagraph() #| Out-Null
+            for ($i = 0; $i -lt $Text.Length; $i++) {
                 # Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine
-                $p = $p.Append($Text[$i]).FontSize($FontSize[$i]) #| Out-Null
+                $p = $p.Append($Text[$i]).FontSize($FontSize[$i]).Color($Color[$i]) #| Out-Null
             }
         } else {
             for ($i = 1; $i -lt $Text.Length; $i++) {
