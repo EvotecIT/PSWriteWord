5d45df631f7689b1c85fcae8fe097d62ee6e54d7
PrzemyslawKlys <pklys@evotec.pl>
2018-06-21 13:24:45 +0200

    Add-WordTOC support

---
+++ Public/PSWordTOC.ps1
@@ -0,0 +1,34 @@
+function Add-WordTOC {
+    [CmdletBinding()]
+    param (
+        [Xceed.Words.NET.Container] $WordDocument,
+        [Xceed.Words.NET.InsertBeforeOrAfter] $BeforeParagraph,
+        [string] $Title = 'Table of contents',
+        [TableOfContentsSwitches[]] $Switches = @(),
+        [HeadingType] $HeaderStyle = [HeadingType]::Heading1,
+        [int] $MaxIncludeLevel = 3,
+        [int] $RightTabPos = $null,
+        [bool] $Supress = $true
+    )
+
+    #public TableOfContents InsertTableOfContents( string title, TableOfContentsSwitches switches, string headerStyle = null, int maxIncludeLevel = 3, int? rightTabPos = null )
+    #public TableOfContents InsertTableOfContents( Paragraph reference, string title, TableOfContentsSwitches switches, string headerStyle = null, int maxIncludeLevel = 3, int? rightTabPos = null )
+    $TableOfContentSwitch = 0;
+    foreach ($S in $switches) {
+        $TableOfContentSwitch += $s -As [Int]
+    }
+    Write-Verbose $TableOfContentSwitch
+
+    #if ($Title -eq $null -or $Title -eq '') {
+    #    $Toc = $WordDocument.InsertDefaultTableOfContents()
+    #    if ($Supress -eq $false) { return $Toc } else { return }
+    #}
+    if ($BeforeParagraph -eq $null) {
+        $toc = $WordDocument.InsertTableOfContents($Title, $TableOfContentSwitch, $HeaderStyle, $MaxIncludeLevel, $RightTabPos)
+    } else {
+        $toc = $WordDocument.InsertTableOfContents($BeforeParagraph, $Title, $TableOfContentSwitch, $HeaderStyle, $MaxIncludeLevel, $RightTabPos)
+    }
+
+    if ($Supress -eq $false) { return $Toc } else { return }
+
+}
