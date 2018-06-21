5693ff2f58f5695194395cbbfcd29c1532ef454d
PrzemyslawKlys <pklys@evotec.pl>
2018-06-21 13:24:23 +0200

    Enum Definiton

--- Enums/TableOfContentsSwitches.ps1
+++ Enums/TableOfContentsSwitches.ps1
@@ -1,56 +1,19 @@
-<#
-[Flags]
-  public enum TableOfContentsSwitches
-  {
-    None = 0 << 0,
-
-    [Description("\\a")]
-    A = 1 << 0,
-
-    [Description("\\b")]
-    B = 1 << 1,
-
-    [Description("\\c")]
-    C = 1 << 2,
-
-    [Description("\\d")]
-    D = 1 << 3,
-
-    [Description("\\f")]
-    F = 1 << 4,
-
-    [Description("\\h")]
-    H = 1 << 5,
-
-    [Description("\\l")]
-    L = 1 << 6,
-
-    [Description("\\n")]
-    N = 1 << 7,
-
-    [Description("\\o")]
-    O = 1 << 8,
-
-    [Description("\\p")]
-    P = 1 << 9,
-
-    [Description("\\s")]
-    S = 1 << 10,
-
-    [Description("\\t")]
-    T = 1 << 11,
-
-    [Description("\\u")]
-    U = 1 << 12,
-
-    [Description("\\w")]
-    W = 1 << 13,
-
-    [Description("\\x")]
-    X = 1 << 14,
-
-    [Description("\\z")]
-    Z = 1 << 15
-  }
-
-#>
+enum TableOfContentsSwitches {
+    None = 0 -shl 0
+    A = 1 -shl 0
+    B = 1 -shl 1
+    C = 1 -shl 2
+    D = 1 -shl 3
+    F = 1 -shl 4
+    H = 1 -shl 5
+    L = 1 -shl 6
+    N = 1 -shl 7
+    O = 1 -shl 8
+    P = 1 -shl 9
+    S = 1 -shl 10
+    T = 1 -shl 11
+    U = 1 -shl 12
+    W = 1 -shl 13
+    X = 1 -shl 14
+    Z = 1 -shl 15
+}
