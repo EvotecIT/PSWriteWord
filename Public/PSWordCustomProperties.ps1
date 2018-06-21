717d70bff0fa08b5b47a2db4f9d735265618b329
PrzemyslawKlys <pklys@evotec.pl>
2018-06-21 18:35:29 +0200

    More changes

---
+++ Public/PSWordCustomProperties.ps1
@@ -0,0 +1,6 @@
+function Add-CustomProperty {
+    param(
+
+
+    )
+}
--- Public/PSWordMargins.ps1
+++ Public/PSWordMargins.ps1
@@ -1,3 +1,32 @@
+<#
+    public static void Margins()
+    {
+      Console.WriteLine( "\tMargins()" );
+
+      // Create a document.
+      using( DocX document = DocX.Create( MarginSample.MarginSampleOutputDirectory + @"Margins.docx" ) )
+      {
+        // Add a title.
+        document.InsertParagraph( "Document margins" ).FontSize( 15d ).SpacingAfter( 50d ).Alignment = Alignment.center;
+
+        // Set the page width to be smaller.
+        document.PageWidth = 350f;
+
+        // Set the document margins.
+        document.MarginLeft = 85f;
+        document.MarginRight = 85f;
+        document.MarginTop = 0f;
+        document.MarginBottom = 50f;
+
+        // Add a paragraph. It will be affected by the document margins.
+        var p = document.InsertParagraph("This is a paragraph from a document with a left margin of 85, a right margin of 85, a top margin of 0 and a bottom margin of 50.");
+
+        document.Save();
+        Console.WriteLine( "\tCreated: Margins.docx\n" );
+      }
+    }
+#>
+
 function Set-WordMargins {
     param(
 
