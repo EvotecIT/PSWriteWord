e058a442189acd637f3641da6e9b232f1137fa7d
PrzemyslawKlys <pklys@evotec.pl>
2018-06-22 12:55:20 +0200

    Basic chartrs support

--- Public/PSWordCharts.ps1
+++ Public/PSWordCharts.ps1
@@ -9,7 +9,7 @@ function Add-WordPieChart {
         [bool] $ChartLegendOverlay = $false
     )
 
-    $Series = Add-Series -ChartName $ChartName -Names $Names -Values $Values
+    $Series = Add-WordChartSeries -ChartName $ChartName -Names $Names -Values $Values
 
     [Xceed.Words.NET.PieChart] $chart = New-Object -TypeName Xceed.Words.NET.PieChart
     $chart.AddLegend($ChartLegendPosition, $ChartLegendOverlay)
@@ -19,7 +19,61 @@ function Add-WordPieChart {
 
 }
 
-function Add-Series {
+function Add-WordLineChart {
+    [CmdletBinding()]
+    param (
+        [Xceed.Words.NET.Container]$WordDocument,
+        [string] $ChartName,
+        [string[]] $Names,
+        [int[]] $Values,
+        [Xceed.Words.NET.Series[]] $ChartSeries,
+        [ChartLegendPosition] $ChartLegendPosition = [ChartLegendPosition]::Left,
+        [bool] $ChartLegendOverlay = $false
+    )
+
+    if ($ChartSeries -eq $null) {
+        $ChartSeries = Add-WordChartSeries -ChartName $ChartName -Names $Names -Values $Values
+    }
+
+    [Xceed.Words.NET.LineChart] $chart = New-Object -TypeName Xceed.Words.NET.LineChart
+    $chart.AddLegend($ChartLegendPosition, $ChartLegendOverlay)
+    foreach ($series in $ChartSeries) {
+        $chart.AddSeries($Series)
+    }
+    $WordDocument.InsertChart($chart)
+}
+
+function Add-WordBarChart {
+    [CmdletBinding()]
+    param (
+        [Xceed.Words.NET.Container]$WordDocument,
+        [string] $ChartName,
+        [string[]] $Names,
+        [int[]] $Values,
+        [Xceed.Words.NET.Series[]] $ChartSeries,
+        [ChartLegendPosition] $ChartLegendPosition = [ChartLegendPosition]::Left,
+        [bool] $ChartLegendOverlay = $false,
+        [BarGrouping] $BarGrouping = [BarGrouping]::Standard,
+        [BarDirection] $BarDirection = [BarDirection]::Bar,
+        [int] $BarGapWidth = 200
+    )
+
+    if ($ChartSeries -eq $null) {
+        $ChartSeries = Add-WordChartSeries -ChartName $ChartName -Names $Names -Values $Values
+    }
+
+    [Xceed.Words.NET.BarChart] $chart = New-Object -TypeName Xceed.Words.NET.BarChart
+    $chart.BarDirection = $BarDirection
+    $chart.BarGrouping = $BarGrouping
+    $chart.GapWidth = $BarGapWidth
+    $chart.AddLegend($ChartLegendPosition, $ChartLegendOverlay)
+    foreach ($series in $ChartSeries) {
+        $chart.AddSeries($Series)
+    }
+    $WordDocument.InsertChart($chart)
+}
+
+function Add-WordChartSeries {
     param (
         [string] $ChartName = 'Legend',
         [string[]] $Names,
