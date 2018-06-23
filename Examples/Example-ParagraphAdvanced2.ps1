b8e456ebd6e55ac60eb5db223399cae26d7adc88
PrzemyslawKlys <pklys@evotec.pl>
2018-06-23 10:42:28 +0200

    Language Example Updates

--- Examples/Example-ParagraphAdvanced2.ps1
+++ Examples/Example-ParagraphAdvanced2.ps1
@@ -1,13 +1,14 @@
+
 Import-Module PSWriteWord -Force
 
-$FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-ParagraphAdvanced1.docx"
+$FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-ParagraphAdvanced2.docx"
 
 $WordDocument = New-WordDocument $FilePath
-Add-WordText -WordDocument $WordDocument -Text 'This is text that has font size of 15', ' and this is font size of 10 ', ' while this will be 12.' `
+Add-WordText -WordDocument $WordDocument -Text 'This is text that has font size of 15', ' and this is font size of 11 ', ' while this will be 12.' `
     -FontSize 15, 10 `
     -Color Blue, Red `
     -Bold $true, $false, $true `
-    -Italic $true, $true
+    -Italic $true, $true  -Language 'en-US', 'en-US', 'en-US'
 
 
 Add-WordText -WordDocument $WordDocument -Text 'This is text that has font size of 15', ' and this is font size of 10 ', ' while this will be 12.' `
@@ -21,10 +22,19 @@ Add-WordText -WordDocument $WordDocument -Text 'This is text that has font size 
     -Highlight darkCyan `
     -SpacingBefore 15, 50 `
     -Spacing 5, 20, 1 `
-    -NewLine $true, $true
+    -NewLine $true, $true  -Language 'en-US', 'en-US', 'en-US'
+
 
 Add-WordText -WordDocument $WordDocument -Text 'This is text that has font size of 15', ' and this is font size of 10 ', ' while this will be default size.' `
     -FontSize 15, 10 `
     -Color Blue, Red `
-    -NewLine $true, $true
-Save-WordDocument $WordDocument -Language 'en-US' -Verbose
+    -NewLine $true, $true -Language 'en-US', 'en-US', 'en-US'
+
+
+Add-WordText -WordDocument $WordDocument -Text 'To jest polska wersja tekstu.' `
+    -FontSize 15, 10 `
+    -Color Blue, Red `
+    -NewLine $true, $true -Language 'pl-PL'
+
+
+Save-WordDocument -WordDocument $WordDocument
