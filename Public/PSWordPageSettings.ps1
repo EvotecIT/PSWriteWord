aa821a265e9386ffa8dea78979b09f7488b27624
PrzemyslawKlys <pklys@evotec.pl>
2018-06-25 18:56:33 +0200

    update on text

--- Public/PSWordPageSettings.ps1
+++ Public/PSWordPageSettings.ps1
@@ -1,12 +1,12 @@
 <#
 
-Pay version 1.5 support this:
+Pay version of Xceed 1.5 support this:
 
 In Section, setting the page orientation individually for the different sections will now be supported.
 In Section, the following properties can now be set to configure each section of the Document: Headers, Footers, DifferentFirstPage, MarginTop, MarginBottom, MarginLeft, MarginRight, MarginHeader, MarginFooter, MirrorMargins, PageWidth, PageHeight, PageBorders, PageLayout.
 In Section, the SectionBreakType property will now correctly get/set the Xml and therefore contain the desired value.
 
-Free version doesn't yet. Therefore orientation, page margins etc can only be applied globally.
+Free version (currently at 1.1 of Xceed) doesn't yet. Therefore orientation, page margins etc can only be applied globally.
 #>
 
 function Set-WordPageSettings {
