800b1fcb5fe5a0121c9d20727ddc8ec191696b56
PrzemyslawKlys <pklys@evotec.pl>
2018-06-25 15:15:56 +0200

    Few new commandlets

--- Public/PSWordPageSettings.ps1
+++ Public/PSWordPageSettings.ps1
@@ -7,20 +7,62 @@ function Set-WordPageSettings {
         [nullable[single]] $MarginTop,
         [nullable[single]] $MarginBottom,
         [nullable[single]] $PageWidth,
-        [nullable[single]] $PageHeight
-        #[alias ("PageLayout")][Orientation] $Orientation
+        [nullable[single]] $PageHeight,
+        [alias ("PageLayout")][nullable[Orientation]] $Orientation
     )
 
-    if ($MarginLeft -ne $null) {
-        Write-Verbose "Set-WordMargins - $MarginLeft"
-        $WordDocument.MarginLeft = $MarginLeft
+    Set-WordMargins -WordDocument $WordDocument -MarginLeft $MarginLeft -MarginRight $MarginRight -MarginTop $MarginTop -MarginBottom $Mar
+    Set-WordPageSize -WordDocument $WordDocument -PageWidth $PageWidth -PageHeight $PageHeight
+    Set-WordOrientation -WordDocument $WordDocument -Orientation $Orientation
+}
+
+function Get-WordPageSettings {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument
+    )
+
+    $Object = [ordered]@{
+        MarginLeft   = $WordDocument.MarginLeft
+        MarginRight  = $WordDocument.MarginRight
+        MarginTop    = $WordDocument.MarginTop
+        MarginBottom = $WordDocument.MarginBottom
+        PageWidth    = $WordDocument.PageWidth
+        PageHeight   = $WordDocument.PageHeight
+        Orientation  = $WordDocument.PageLayout.Orientation
     }
+    return $Object
+}
+
+function Set-WordOrientation {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument,
+        [alias ("PageLayout")][Orientation] $Orientation
+    )
+    if ($Orientation -ne $null) { $WordDocument.PageLayout.Orientation = $Orientation }
+}
+function Set-WordMargins {
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument,
+        [nullable[single]] $MarginLeft,
+        [nullable[single]] $MarginRight,
+        [nullable[single]] $MarginTop,
+        [nullable[single]] $MarginBottom
+    )
+
+    if ($MarginLeft -ne $null) { $WordDocument.MarginLeft = $MarginLeft }
     if ($MarginRight -ne $null) {$WordDocument.MarginRight = $MarginRight }
     if ($MarginTop -ne $null) {$WordDocument.MarginTop = $MarginTop }
     if ($MarginBottom -ne $null) {$WordDocument.MarginBottom = $MarginBottom }
+}
+function Set-WordPageSize {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument,
+        [nullable[single]] $PageWidth,
+        [nullable[single]] $PageHeight
+    )
     if ($PageWidth -ne $null) {$WordDocument.PageWidth = $PageWidth }
     if ($PageHeight -ne $null) {$WordDocument.PageHeight = $PageHeight }
-    #if ($Orientation -ne $null) { $WordDocument.Orientation = $Orientation }
-
-
 }
