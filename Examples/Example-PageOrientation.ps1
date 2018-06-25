267d92cf7ff2bafc39d072936569f26433413732
PrzemyslawKlys <pklys@evotec.pl>
2018-06-25 15:15:37 +0200

    Orientation Example

--- Examples/Example-PageOrientation.ps1
+++ Examples/Example-PageOrientation.ps1
@@ -1,7 +1,23 @@
-<#
-    Work in progress. Doesn't work.
-#>
-
-Import-Module PSWriteWord -Force
+Import-Module PSWriteWord #-Force
 
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-PageOrientation1.docx"
+
+$WordDocument = New-WordDocument $FilePath
+### set orientation
+Set-WordPageSettings -WordDocument $WordDocument -Orientation Landscape
+
+### alternatively you can use this commandlet
+Set-WordOrientation -WordDocument $WordDocument -Orientation Landscape
+
+### add 3 paragraphs
+Add-WordText -WordDocument $WordDocument -Text 'This is a text' -FontSize 10
+Add-WordText -WordDocument $WordDocument -Text 'This is a text font size 21' -FontSize 21
+Add-WordText -WordDocument $WordDocument -Text 'This is a text font size 15' -FontSize 15
+
+### get page settings
+Get-WordPageSettings -WordDocument $WordDocument
+### Save document
+Save-WordDocument -WordDocument $WordDocument
+
+### Start Word with file
+Invoke-Item $FilePath
