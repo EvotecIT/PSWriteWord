9c16f408d0ba567b984096b748b3d34f02d32b94
PrzemyslawKlys <pklys@evotec.pl>
2018-06-26 18:28:31 +0200

    Set-WordPicture

--- Public/PSWordPicture.ps1
+++ Public/PSWordPicture.ps1
@@ -73,3 +73,27 @@ function Remove-WordPicture {
         $Paragraph.Pictures[$PictureID].Remove()
     }
 }
+
+function Set-WordPicture {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument,
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.InsertBeforeOrAfter] $Paragraph,
+        [Xceed.Words.NET.DocXElement] $Picture,
+        [string] $ImagePath,
+        [int] $Rotation,
+        [switch] $FlipHorizontal,
+        [switch] $FlipVertical,
+        [int] $ImageWidth,
+        [int] $ImageHeight,
+        [string] $Description,
+        [int] $PictureID,
+        [bool] $Supress = $false
+    )
+    Remove-WordPicture -WordDocument $WordDocument -Paragraph $Paragraph
+    $data = Add-WordPicture -WordDocument $WordDocument -Paragraph $Paragraph `
+        -Picture $Picture `
+        -ImagePath $ImagePath -ImageWidth $ImageWidth -ImageHeight $ImageHeight `
+        -Rotation $Rotation -FlipHorizontal:$FlipHorizontal -FlipVertical:$FlipVertical -Supress $Supress
+    return $data
+}
