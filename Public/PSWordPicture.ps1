2a20ec73bec3d0dd46789ed3b0ebf48d93e10f45
PrzemyslawKlys <pklys@evotec.pl>
2018-06-26 18:09:24 +0200

    More Picture Commandlets support

--- Public/PSWordPicture.ps1
+++ Public/PSWordPicture.ps1
@@ -38,11 +38,38 @@ function Get-WordPicture {
         [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument,
         [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.InsertBeforeOrAfter] $Paragraph,
         [switch] $ListParagraphs,
+        [switch] $ListPictures,
+        [nullable[int]] $PictureID
+    )
+    if ($ListParagraphs -eq $true -and $ListPictures -eq $true) {
+        throw 'Only one option is possible at time (-ListParagraphs or -ListPictures)'
+    }
+    if ($ListParagraphs) {
+        $List = New-ArrayList
+        $Paragraphs = $WordDocument.Paragraphs
+        foreach ($p in $Paragraphs) {
+            if ($p.Pictures -ne $null) {
+                Add-ToArray -List $List -Element $p
+            }
+        }
+        return $List
+    }
+    if ($ListPictures) {
+        return $WordDocument.Pictures
+    }
+    if ($PictureID -ne $null) {
+        return $WordDocument.Pictures[$PictureID]
+    }
+}
+
+function Remove-WordPicture {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container]$WordDocument,
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.InsertBeforeOrAfter] $Paragraph,
         [int] $PictureID
     )
-    if ($ListParagraphs) {
-        return $WordDocument.Pictures
-    } else {
-        return $WordDocument.Pictures[$PictureID]
+    if ($Paragraph.Pictures[$PictureID] -ne $null) {
+        $Paragraph.Pictures[$PictureID].Remove()
     }
 }
