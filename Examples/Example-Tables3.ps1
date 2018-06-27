414cfb8adf79f0896965397b860c3f3763180b92
PrzemyslawKlys <pklys@evotec.pl>
2018-06-27 14:21:00 +0200

    Table example

--- Examples/Example-Tables3.ps1
+++ Examples/Example-Tables3.ps1
@@ -1,4 +1,4 @@
-Import-Module PSWriteWord -Force
+Import-Module PSWriteWord #-Force
 
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-Tables3.docx"
 
@@ -6,20 +6,35 @@ $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-Tables3.docx"
 $WordDocument = New-WordDocument $FilePath
 
 $InvoiceEntry1 = @{}
-$InvoiceEntry1.Description = 'IT Services'
+$InvoiceEntry1.Description = 'IT Services 1'
 $InvoiceEntry1.Amount = '$200'
 
 $InvoiceEntry2 = @{}
-$InvoiceEntry2.Description = 'IT Services'
-$InvoiceEntry2.Amount = '$200'
+$InvoiceEntry2.Description = 'IT Services 2'
+$InvoiceEntry2.Amount = '$300'
+
+$InvoiceEntry3 = @{}
+$InvoiceEntry3.Description = 'IT Services 3'
+$InvoiceEntry3.Amount = '$288'
+
+$InvoiceEntry4 = @{}
+$InvoiceEntry4.Description = 'IT Services 4'
+$InvoiceEntry4.Amount = '$301'
+
+$InvoiceEntry5 = @{}
+$InvoiceEntry5.Description = 'IT Services 5'
+$InvoiceEntry5.Amount = '$299'
 
 $InvoiceData = @()
 $InvoiceData += $InvoiceEntry1
 $InvoiceData += $InvoiceEntry2
+$InvoiceData += $InvoiceEntry3
+$InvoiceData += $InvoiceEntry4
+$InvoiceData += $InvoiceEntry5
 
-Add-WordText -WordDocument $WordDocument -Text "Invoice Data" -FontSize 15
+Add-WordText -WordDocument $WordDocument -Text "Invoice Data" -FontSize 15 -Alignment center
 Add-WordParagraph -WordDocument $WordDocument
-Add-WordTable -WordDocument $WordDocument -Table $InvoiceData -Design LightShading  -Verbose
+Add-WordTable -WordDocument $WordDocument -Table $InvoiceData -Design LightShading # -Verbose
 
 Save-WordDocument $WordDocument
 
