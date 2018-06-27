4b41ca4f2803a99f7bc2f4843d8f8e957e8c4af2
PrzemyslawKlys <pklys@evotec.pl>
2018-06-27 19:42:15 +0200

    Template 1st commit

---
+++ Examples/Example-TemplateCreateInvoice3.ps1
@@ -0,0 +1,50 @@
+Import-Module PSWriteWord -Force
+
+### Before running this script make sure to run Example-CreateWord first
+$FilePathTemplate = "$PSScriptRoot\Templates\WordTemplate-InvoiceWithLogo.docx"
+$FilePathInvoice = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-TemplateCreateInvoice3.docx"
+
+$FilePathImage = "$PSScriptRoot\Images\Logo-Evotec-Small.jpg"
+
+$WordDocument = Get-WordDocument -FilePath $FilePathTemplate
+
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanyName'  -Value 'Evotec'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanySlogan'  -Value 'IT Consultants'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanyStreetName'  -Value 'Francuska 96B/23'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanyCity'  -Value 'Katowice'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanyZipCode'  -Value '40-507'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanyPhone'  -Value '+48 500 500 500'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'CompanySupport'  -Value 'fake-email@evotec1.xyz'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'ClientName'  -Value 'Fake Company'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'ClientStreetName'  -Value 'Fake Street Name'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'ClientCity'  -Value 'Warsaw'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'ClientZipCode'  -Value '10-000'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'ClientPhone'  -Value '+48 400 400 400'
+Add-WordCustomProperty -WordDocument $WordDocument -Name 'ClientMail'  -Value 'fake-email@fake-company.com'
+
+$ParagraphsWithPictures = Get-WordPicture -WordDocument $WordDocument -ListParagraphs
+Set-WordPicture -WordDocument $WordDocument -Paragraph $ParagraphsWithPictures[0] -ImagePath $FilePathImage -ImageWidth 100 -ImageHeight 40
+
+
+$InvoiceEntry1 = @{}
+$InvoiceEntry1.Description = 'IT Services 1'
+$InvoiceEntry1.Amount = '$200'
+
+$InvoiceEntry2 = @{}
+$InvoiceEntry2.Description = 'IT Services 1'
+$InvoiceEntry2.Amount = '$200'
+
+$InvoiceData = @()
+$InvoiceData += $InvoiceEntry1
+$InvoiceData += $InvoiceEntry2
+
+
+$LastTable = Get-WordTable -WordDocument $WordDocument -LastTable
+$RowsToRemove = $LastTable.Rows.Count - 1
+Remove-WordTableRow -Table $LastTable -Count $RowsToRemove
+
+
+
+Save-WordDocument -WordDocument $WordDocument -FilePath $FilePathInvoice
+### Start Word with file
+Invoke-Item $FilePathInvoice
