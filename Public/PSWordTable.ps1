c07da51724b7421ef87b0089ae3e6e299dd38aef
PrzemyslawKlys <pklys@evotec.pl>
2018-06-27 18:18:19 +0200

    More work on Tables

--- Public/PSWordTable.ps1
+++ Public/PSWordTable.ps1
@@ -244,10 +244,58 @@ function Add-WordTableRow {
     [CmdletBinding()]
     param (
         [Xceed.Words.NET.InsertBeforeOrAfter] $Table,
-        [int] $Count = 1
+        [int] $Count = 1,
+        [nullable[int]] $Index
     )
-    for ($i = 0; $i -le $Count; $i++) {
-        $Table.InsertRow()
+    if ($Table -ne $null) {
+        if ($Index -ne $null) {
+            for ($i = 0; $i -lt $Count; $i++) {
+                $Table.InsertRow($Index + $i)
+            }
+        } else {
+            for ($i = 0; $i -lt $Count; $i++) {
+                $Table.InsertRow()
+            }
+        }
+    }
+}
+
+function Remove-WordTableRow {
+    [CmdletBinding()]
+    param (
+        [Xceed.Words.NET.InsertBeforeOrAfter] $Table,
+        [int] $Count = 1,
+        [nullable[int]] $Index
+    )
+    if ($Table -ne $null) {
+        if ($Index -ne $null) {
+            for ($i = 0; $i -lt $Count; $i++) {
+                $Table.RemoveRow($Index + $i)
+            }
+        } else {
+            for ($i = 0; $i -lt $Count; $i++) {
+                $Table.RemoveRow()
+            }
+        }
+    }
+}
+function Remove-WordTableColumn {
+    [CmdletBinding()]
+    param (
+        [Xceed.Words.NET.InsertBeforeOrAfter] $Table,
+        [int] $Count = 1,
+        [nullable[int]] $Index
+    )
+    if ($Table -ne $null) {
+        if ($Index -ne $null) {
+            for ($i = 0; $i -lt $Count; $i++) {
+                $Table.RemoveColumn($Index + $i)
+            }
+        } else {
+            for ($i = 0; $i -lt $Count; $i++) {
+                $Table.RemoveColumn()
+            }
+        }
     }
 }
 
