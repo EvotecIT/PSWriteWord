c001046c777e814cd12d7b70860b40b2a60b953a
PrzemyslawKlys <pklys@evotec.pl>
2018-06-27 12:51:39 +0200

    Example tables

---
+++ Examples/Example-Tables7.ps1
@@ -0,0 +1,22 @@
+Import-Module PSWriteWord -Force
+
+$FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-Tables7.docx"
+
+#Clear-Host
+$WordDocument = New-WordDocument $FilePath
+
+$InvoiceEntry1 = @{ Description = 'IT Services 1'; Amount = '$230' }
+$InvoiceEntry1 = @{ Description = 'IT Services 2'; Amount = '$200' }
+
+$InvoiceData = @()
+$InvoiceData += $InvoiceEntry1
+$InvoiceData += $InvoiceEntry2
+
+Add-WordText -WordDocument $WordDocument -Text "Invoice Data" -FontSize 15
+Add-WordParagraph -WordDocument $WordDocument
+Add-WordTable -WordDocument $WordDocument -Table $InvoiceData -Design LightShading  -Verbose
+
+Save-WordDocument $WordDocument
+
+### Start Word with file
+Invoke-Item $FilePath
