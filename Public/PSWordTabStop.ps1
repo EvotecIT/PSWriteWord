082e3ebd316aa39c89426f721bd55061954c8468
PrzemyslawKlys <pklys@evotec.pl>
2018-07-02 16:11:25 +0200

    Cleanup of Add-WordTabStopPosition

--- Public/PSWordTabStop.ps1
+++ Public/PSWordTabStop.ps1
@@ -8,17 +8,8 @@ function Add-WordTabStopPosition {
         [Alignment] $Alignment,
         [bool] $Supress = $true
     )
-    #<summary>
-    #Add a new TabStopPosition in the current paragraph.
-    #</summary>
-    #param name="alignment">Specifies the alignment of the Tab stop.</param>
-    #param name="position">Specifies the horizontal position of the tab stop.</param>
-    #param name="leader">Specifies the character used to fill in the space created by a tab.</param>
-    #<returns>The modified Paragraph.</returns>
-
     if ($Paragraph -eq $null) {
         $Paragraph = Add-WordParagraph -WordDocument $WordDocument -Supress $False
     }
     $Paragraph.InsertTabStopPosition($Alignment, $HorizontalPosition, $TabStopPositionLeader)
-
 }
