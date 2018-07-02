bf7000aefb156e00547f19134aa9380fe1551eb9
PrzemyslawKlys <pklys@evotec.pl>
2018-07-02 15:31:23 +0200

    Expanded Example for Lines (new version 1.2)

--- Examples/Example-AddLines1.ps1
+++ Examples/Example-AddLines1.ps1
@@ -3,15 +3,26 @@ Import-Module PSWriteWord #-Force
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-AddLines1.docx"
 
 $WordDocument = New-WordDocument $FilePath
-Add-WordText -WordDocument $WordDocument -Text 'This is a text' -FontSize 10
-
+Add-WordText -WordDocument $WordDocument -Text 'Adding line that should be red and double. Horizonal Border Position by default bottom' -FontSize 10
 Add-WordLine -WordDocument $WordDocument -LineColor Red -LineType double
 
-Add-WordText -WordDocument $WordDocument -Text 'This is a text font size 21' -FontSize 21
-
+Add-WordText -WordDocument $WordDocument -Text 'Adding line that should be blue and single. Horizontal Border Position by default bottom' -FontSize 10
 Add-WordLine -WordDocument $WordDocument -LineColor Blue -LineType single -LineSize 10
 
-Add-WordText -WordDocument $WordDocument -Text 'This is a text font size 15' -FontSize 15
+Add-WordText -WordDocument $WordDocument -Text 'Adding line that should be red and triple. Horizontal Border Position by default bottom' -FontSize 10
+Add-WordLine -WordDocument $WordDocument -LineColor Red -LineType triple
+
+Add-WordText -WordDocument $WordDocument -Text 'Adding line that should be blue and single with Horizonal Border Position top' -FontSize 10
+Add-WordLine -WordDocument $WordDocument -HorizontalBorderPosition top -LineColor Blue -LineType single -LineSize 10
+
+Add-WordText -WordDocument $WordDocument -Text 'Adding line that should be red and triple with Horizonal Border Position bottom' -FontSize 10
+Add-WordLine -WordDocument $WordDocument -HorizontalBorderPosition bottom -LineColor Red -LineType triple
+
+Add-WordText -WordDocument $WordDocument -Text  'Adding line that should be blue and single with Horizonal Border Position top' -FontSize 10
+Add-WordLine -WordDocument $WordDocument -HorizontalBorderPosition top -LineColor Blue -LineType single -LineSize 10
+
+Add-WordText -WordDocument $WordDocument -Text  'Adding line that should be blue and single with Horizonal Border Position bottom' -FontSize 10
+Add-WordLine -WordDocument $WordDocument -HorizontalBorderPosition bottom -LineColor Blue -LineType single -LineSize 10
 
 Save-WordDocument $WordDocument -Language 'en-US'
 
