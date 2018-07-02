59eb4806efc91dc444874f3f2f54e83d43d6c814
PrzemyslawKlys <pklys@evotec.pl>
2018-07-02 17:33:50 +0200

    Add-WordTabStopPosition Example

--- Examples/Example-CreateWord5.ps1
+++ Examples/Example-CreateWord5.ps1
@@ -8,8 +8,14 @@ $WordDocument = New-WordDocument $FilePath
 $WordDocument | Add-WordText -Text 'This is a text' -FontSize 10
 $WordDocument | Add-WordText -Text 'This is a text font size 21' -FontSize 21
 $WordDocument | Add-WordText -Text 'This is a text font size 15' -FontSize 15
+
+$Paragraph = Add-WordParagraph -WordDocument $WordDocument -Supress $false
+$Paragraph = Add-WordTabStopPosition -Paragraph $Paragraph -TabStopPositionLeader dot -HorizontalPosition 216 -Alignment center -Supress $false
+$Paragraph = Add-WordTabStopPosition -Paragraph $Paragraph -TabStopPositionLeader dot -HorizontalPosition 432 -Alignment right -Supress $false
+$Paragraph = Add-WordText -Paragraph $Paragraph -Text "Tab stop position on Left`t Middle `t and Right" -FontSize 15
+
 ### Save document
-$WordDocument |Save-WordDocument
+$WordDocument |Save-WordDocument -Language 'en-US'
 
 ### Start Word with file
 Invoke-Item $FilePath
