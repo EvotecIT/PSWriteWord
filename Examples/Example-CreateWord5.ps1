2154740ba54042376cc2ea6d3110bcefd910cf2c
PrzemyslawKlys <pklys@evotec.pl>
2018-07-06 20:58:01 +0200

    Some update to example for more tests

--- Examples/Example-CreateWord5.ps1
+++ Examples/Example-CreateWord5.ps1
@@ -1,4 +1,4 @@
-Import-Module PSWriteWord #-Force
+Import-Module PSWriteWord -Force
 
 $FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-CreateWord5.docx"
 
@@ -12,7 +12,16 @@ $WordDocument | Add-WordText -Text 'This is a text font size 15' -FontSize 15
 $Paragraph = Add-WordParagraph -WordDocument $WordDocument -Supress $false
 $Paragraph = Add-WordTabStopPosition -Paragraph $Paragraph -TabStopPositionLeader dot -HorizontalPosition 216 -Alignment center -Supress $false
 $Paragraph = Add-WordTabStopPosition -Paragraph $Paragraph -TabStopPositionLeader dot -HorizontalPosition 432 -Alignment right -Supress $false
-$Paragraph = Add-WordText -Paragraph $Paragraph -Text "Tab stop position on Left`t Middle `t and Right" -FontSize 15
+$Paragraph = Add-WordText -Paragraph $Paragraph -Text "Tab stop position on Left`t Middle `t and Right" -FontSize 15 -Supress $false
+
+### adds green color to paragraph above
+Set-WordText -Paragraph $Paragraph -Color Green -FontSize 30 -Verbose
+
+### adds another empty paragraph
+$Paragraph = Add-WordParagraph -WordDocument $WordDocument -Supress $false
+
+### adds 3 texts to to an empty paragraph from above
+Add-WordText -Paragraph $Paragraph -Color Green -FontSize 30 -bold $null, $null, $true -Text 'Font size 30', ' not font size 30', ' not font size 30 but bold'
 
 ### Save document
 $WordDocument |Save-WordDocument -Language 'en-US'
