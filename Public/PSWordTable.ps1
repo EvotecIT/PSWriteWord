b901b50144d4a74784976bc6e25af507e28dd607
PrzemyslawKlys <pklys@evotec.pl>
2018-07-07 13:38:28 +0200

    ContinueFormatting

--- Public/PSWordTable.ps1
+++ Public/PSWordTable.ps1
@@ -438,35 +438,31 @@ function Set-WordTableContinueFormatting {
         [Script[]] $Script = @()
     )
     for ($RowNr = 0; $RowNr -lt $NumberRows; $RowNr++) {
-        Write-Verbose "Add-WordColor - RowNr: $RowNr / $NumberRows"
-        if ($null -eq $Color[$RowNr]) { $Color += $Color[$RowNr - 1] }
-        if ($null -eq $FontSize[$RowNr]) { $FontSize += $FontSize[$RowNr - 1] }
-        if ($null -eq $FontFamily[$RowNr]) { $FontFamily += $FontFamily[$RowNr - 1] }
-        if ($null -eq $Bold[$RowNr]) { $Bold += $Bold[$RowNr - 1] }
-        if ($null -eq $Italic[$RowNr]) { $Italic += $Italic[$RowNr - 1] }
-        #if ($null -eq $UnderlineStyle[$RowNr]) { $UnderlineStyle += $UnderlineStyle[$RowNr - 1] }
-        #if ($null -eq $UnderlineColor[$RowNr]) { $UnderlineColor += $UnderlineColor[$RowNr - 1] }
-        <#
-    -SpacingAfter $SpacingAfter[$RowNr] `
-    -SpacingBefore $SpacingBefore[$RowNr] `
-    -Spacing $Spacing[$RowNr] `
-    -Highlight $Highlight[$RowNr] `
-    -CapsStyle $CapsStyle[$RowNr] `
-    -StrikeThrough $StrikeThrough[$RowNr] `
-    -HeadingType $HeadingType[$RowNr] `
-    -PercentageScale $PercentageScale[$RowNr] `
-    -Misc $Misc[$RowNr] `
-    -Language $Language[$RowNr]`
-    -Kerning $Kerning[$RowNr]`
-    -Hidden $Hidden[$RowNr]`
-    -Position $Position[$RowNr]`
-    -IndentationFirstLine $IndentationFirstLine[$RowNr]`
-    -IndentationHanging $IndentationHanging[$RowNr]`
-    -Alignment $Alignment[$RowNr]`
-    -DirectionFormatting $DirectionFormatting[$RowNr] `
-    -ShadingType $ShadingType[$RowNr]`
-    -Script $Script[$RowNr]
-    #>
+        Write-Verbose "Set-WordTableContinueFormatting - RowNr: $RowNr / $NumberRows"
+        if ($null -eq $Color[$RowNr] -and $null -ne $Color[$RowNr - 1]) { $Color += $Color[$RowNr - 1] }
+        if ($null -eq $FontSize[$RowNr] -and $null -ne $FontSize[$RowNr - 1]) {  $FontSize += $FontSize[$RowNr - 1]  }
+        if ($null -eq $FontFamily[$RowNr] -and $null -ne $FontFamily[$RowNr - 1]) { $FontFamily += $FontFamily[$RowNr - 1] }
+        if ($null -eq $Bold[$RowNr] -and $null -ne $Bold[$RowNr - 1]) {$Bold += $Bold[$RowNr - 1] }
+        if ($null -eq $Italic[$RowNr] -and $null -ne $Italic[$RowNr - 1]) { $Italic += $Italic[$RowNr - 1] }
+        if ($null -eq $SpacingAfter[$RowNr] -and $null -ne $SpacingAfter[$RowNr - 1]) { $SpacingAfter += $SpacingAfter[$RowNr - 1] }
+        if ($null -eq $SpacingBefore[$RowNr] -and $null -ne $SpacingBefore[$RowNr - 1]) { $SpacingBefore += $SpacingBefore[$RowNr - 1] }
+        if ($null -eq $Spacing[$RowNr] -and $null -ne $Spacing[$RowNr - 1]) { $Spacing += $Spacing[$RowNr - 1] }
+        if ($null -eq $Highlight[$RowNr] -and $null -ne $Highlight[$RowNr - 1]) { $Highlight += $Highlight[$RowNr - 1] }
+        if ($null -eq $CapsStyle[$RowNr] -and $null -ne $CapsStyle[$RowNr - 1]) { $CapsStyle += $CapsStyle[$RowNr - 1] }
+        if ($null -eq $StrikeThrough[$RowNr] -and $null -ne $StrikeThrough[$RowNr - 1]) { $StrikeThrough += $StrikeThrough[$RowNr - 1] }
+        if ($null -eq $HeadingType[$RowNr] -and $null -ne $HeadingType[$RowNr - 1]) { $HeadingType += $HeadingType[$RowNr - 1] }
+        if ($null -eq $PercentageScale[$RowNr] -and $null -ne $PercentageScale[$RowNr - 1]) { $PercentageScale += $PercentageScale[$RowNr - 1] }
+        if ($null -eq $Misc[$RowNr] -and $null -ne $Misc[$RowNr - 1]) { $Misc += $Misc[$RowNr - 1] }
+        if ($null -eq $Language[$RowNr] -and $null -ne $Language[$RowNr - 1]) { $Language += $Language[$RowNr - 1] }
+        if ($null -eq $Kerning[$RowNr] -and $null -ne $Kerning[$RowNr - 1]) { $Kerning += $Kerning[$RowNr - 1] }
+        if ($null -eq $Hidden[$RowNr] -and $null -ne $Hidden[$RowNr - 1]) { $Hidden += $Hidden[$RowNr - 1] }
+        if ($null -eq $Position[$RowNr] -and $null -ne $Position[$RowNr - 1]) { $Position += $Position[$RowNr - 1] }
+        if ($null -eq $IndentationFirstLine[$RowNr] -and $null -ne $IndentationFirstLine[$RowNr - 1]) { $IndentationFirstLine += $IndentationFirstLine[$RowNr - 1] }
+        if ($null -eq $IndentationHanging[$RowNr] -and $null -ne $IndentationHanging[$RowNr - 1]) { $IndentationHanging += $IndentationHanging[$RowNr - 1] }
+        if ($null -eq $Alignment[$RowNr] -and $null -ne $Alignment[$RowNr - 1]) { $Alignment += $Alignment[$RowNr - 1] }
+        if ($null -eq $DirectionFormatting[$RowNr] -and $null -ne $DirectionFormatting[$RowNr - 1]) { $DirectionFormatting += $DirectionFormatting[$RowNr - 1] }
+        if ($null -eq $ShadingType[$RowNr] -and $null -ne $ShadingType[$RowNr - 1]) { $ShadingType += $ShadingType[$RowNr - 1] }
+        if ($null -eq $Script[$RowNr] -and $null -ne $Script[$RowNr - 1]) { $Script += $Script[$RowNr - 1] }
     }
 
     return @(
