25d87804dd5174c9525ca5dccae988e9ea8d1407
PrzemyslawKlys <pklys@evotec.pl>
2018-07-19 12:35:45 +0200

    Tests

--- PSWriteWordTests.ps1
+++ PSWriteWordTests.ps1
@@ -2,11 +2,11 @@ $PSVersionTable.PSVersion
 
 $ModuleVersion = (Get-Content -Raw .\PSWriteWord.psd1)  | Invoke-Expression | ForEach-Object ModuleVersion
 
-$dest = "PSWriteWord-{0}-{1}.zip" -f $ModuleVersion, (Get-Date).ToString("yyyyMMddHHmmss")
-Compress-Archive -Path . -DestinationPath .\$dest
+#$dest = "Builds\PSWriteWord-{0}-{1}.zip" -f $ModuleVersion, (Get-Date).ToString("yyyyMMddHHmmss")
+#Compress-Archive -Path . -DestinationPath .\$dest
 
 if ((Get-Module -ListAvailable pester) -eq $null) {
-    Install-Module -Name Pester -Repository PSGallery -Force
+    Install-Module -Name Pester -Repository PSGallery -Force -SkipPublisherCheck
 }
 
 $result = Invoke-Pester -Script $PSScriptRoot\Tests -Verbose -PassThru
