312e542cf3ac8ae6834475456a58ad103854583a
PrzemyslawKlys <pklys@evotec.pl>
2018-07-19 15:00:14 +0200

    Tests

--- Tests/Add-WordText.Tests.ps1
+++ Tests/Add-WordText.Tests.ps1
@@ -2,11 +2,10 @@
 Import-Module $PSScriptRoot\..\PSWriteWord.psd1 #-Force
 
 Describe 'Add-WordText' {
-    It 'Given -Text parameter should create 1 paragraph with a text field This is text' {
+    It 'Given Text parameter should create 1 paragraph with a text field This is text' {
         $WordDocument = New-WordDocument
         $WordDocument | Add-WordText -Text 'This is text'
         $WordDocument.Paragraphs[0].Text | Should -Be 'This is text'
         $WordDocument.Paragraphs.Count | Should -Be 1
     }
-
 }
