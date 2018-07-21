4ecc86dbc96e37a16f25fbcf548e664187b683c5
PrzemyslawKlys <pklys@evotec.pl>
2018-07-21 10:49:47 +0200

    Updates

--- Public/PSWordTableRows.ps1
+++ Public/PSWordTableRows.ps1
@@ -58,13 +58,19 @@ function Get-WordTableRow {
     [CmdletBinding()]
     param (
         [Xceed.Words.NET.InsertBeforeOrAfter] $Table,
+        [int] $RowNr,
+        [int] $ColumnNr,
+        [int] $ParagraphNr,
         [switch] $RowsCount
     )
 
     if ($Table -ne $null) {
         if ($RowsCount) {
+            # returns INT - Row count number
             return $Table.Rows.Count
         }
+        # returns Paragraph of a Table Row
+        return $Table.Rows[$RowNr].Cells[$ColumnNr].Paragraphs[$ParagraphNr]
     }
 }
 
