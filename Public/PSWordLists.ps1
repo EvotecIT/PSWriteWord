769ac1b337adb8fbe957f5c1acc7cd8d3cb2d876
PrzemyslawKlys <pklys@evotec.pl>
2018-07-22 11:52:21 +0200

    Changes to Add-WordList

--- Public/PSWordLists.ps1
+++ Public/PSWordLists.ps1
@@ -4,62 +4,66 @@ function Add-WordList {
         [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.Container] $WordDocument,
         [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][Xceed.Words.NET.InsertBeforeOrAfter] $Paragraph,
         [ListItemType]$ListType,
-        [string[]] $ListData = $null,
-        $Object = $null,
+        [object] $ListData = $null,
+        [InsertWhere] $InsertWhere = [InsertWhere]::AfterSelf,
         [bool] $Supress = $false
     )
     $LevelPrimary = 0
     $LevelSecondary = 1
     $LevelThird = 2
-    if ($ListData -ne $null) {
-        $ListCount = ($ListData | Measure-Object).Count
-        If ($ListCount -gt 0) {
-            $List = $WordDocument.AddList($ListData[0], 0, $ListType)
-            Write-Verbose "AddList - Name: $($List.GetType().Name) - BaseType: $($List.GetType().BaseType)"
-            for ($i = 1; $i -lt $ListData.Count; $i++ ) {
-                $ListItem = $WordDocument.AddListItem($List, $ListData[$i])
-                Write-Verbose "AddList - Name: $($ListItem.GetType().Name) - BaseType: $($ListItem.GetType().BaseType)"
-            }
-            if ($Paragraph -ne $null) {
-                $data = $Paragraph.InsertList($List)
-            } else {
-                $data = $WordDocument.InsertList($List)
-            }
-        }
-    }
-    if ($Object -ne $null) {
+    Write-Verbose "Add-WordList - InsertType: BaseName: $($ListData.GetType().BaseType) Name: $($ListData.GetType().Name)"
 
-        $IsFirstTitle = $True
-        $Titles = Get-ObjectTitles -Object $Object
-        foreach ($Title in $Titles) {
-            $Values = Get-ObjectData -Object $Object -Title $Title
-            #$Values
-            $IsFirstValue = $True
-            foreach ($Value in $Values) {
-                if ($IsFirstTitle -eq $True) {
-                    $List = $WordDocument.AddList($Value, $LevelPrimary, $ListType)
-                    Write-Verbose "AddList (Object) - Name: $($List.GetType().Name) - BaseType: $($List.GetType().BaseType)"
-                } else {
-                    #Write-Color 'Value IsFirstTitle ', $IsFirstTitle, ' Value IsFirstValue ', $IsFirstValue, ' Count ', $Values.Count, ' Value: ', $Value -Color Yellow, Green, Yellow, Green, White, Yellow
-                    if ($IsFirstValue -eq $True) {
-                        $ListItem = $WordDocument.AddListItem($List, $Value, $LevelPrimary) #> $null
-                        Write-Verbose "AddList (Object) - Name: $($ListItem.GetType().Name) - BaseType: $($ListItem.GetType().BaseType)"
-                    } else {
-                        $ListItem = $WordDocument.AddListItem($List, $Value, $LevelSecondary) # > $null
-                        Write-Verbose "AddList (Object) - Name: $($ListItem.GetType().Name) - BaseType: $($ListItem.GetType().BaseType)"
-                    }
+    $ObjectType = $ListData.GetType().Name
+
+    if ($ListData -ne $null) {
+        if ($ObjectType -eq 'Object[]') {
+            $ListCount = ($ListData | Measure-Object).Count
+            If ($ListCount -gt 0) {
+                $List = $WordDocument.AddList($ListData[0], 0, $ListType)
+                Write-Verbose "AddList - Name: $($List.GetType().Name) - BaseType: $($List.GetType().BaseType)"
+                for ($i = 1; $i -lt $ListData.Count; $i++ ) {
+                    $ListItem = $WordDocument.AddListItem($List, $ListData[$i])
+                    Write-Verbose "AddList - Name: $($ListItem.GetType().Name) - BaseType: $($ListItem.GetType().BaseType)"
                 }
-                $IsFirstTitle = $false
-                $IsFirstValue = $false
             }
+        } else {
+            $IsFirstTitle = $True
+            $Titles = Get-ObjectTitles -Object $ListData
+            foreach ($Title in $Titles) {
+                $Values = Get-ObjectData -Object $ListData -Title $Title
+                #$Values
+                $IsFirstValue = $True
+                foreach ($Value in $Values) {
+                    if ($IsFirstTitle -eq $True) {
+                        $List = $WordDocument.AddList($Value, $LevelPrimary, $ListType)
+                        Write-Verbose "AddList (Object) - Name: $($List.GetType().Name) - BaseType: $($List.GetType().BaseType)"
+                    } else {
+                        #Write-Color 'Value IsFirstTitle ', $IsFirstTitle, ' Value IsFirstValue ', $IsFirstValue, ' Count ', $Values.Count, ' Value: ', $Value -Color Yellow, Green, Yellow, Green, White, Yellow
+                        if ($IsFirstValue -eq $True) {
+                            $ListItem = $WordDocument.AddListItem($List, $Value, $LevelPrimary) #> $null
+                            Write-Verbose "AddList (Object) - Name: $($ListItem.GetType().Name) - BaseType: $($ListItem.GetType().BaseType)"
+                        } else {
+                            $ListItem = $WordDocument.AddListItem($List, $Value, $LevelSecondary) # > $null
+                            Write-Verbose "AddList (Object) - Name: $($ListItem.GetType().Name) - BaseType: $($ListItem.GetType().BaseType)"
+                        }
+                    }
+                    $IsFirstTitle = $false
+                    $IsFirstValue = $false
+                }
+            }
+
         }
         if ($Paragraph -ne $null) {
-            $data = $Paragraph.InsertList($List)
+            if ($InsertWhere -eq [InsertWhere]::AfterSelf) {
+                $data = $Paragraph.InsertListAfterSelf($List)
+            } elseif ($InsertWhere -eq [InsertWhere]::AfterSelf) {
+                $data = $Paragraph.InsertListBeforeSelf($List)
+            }
         } else {
-            $data = $WordDocument.InsertList($List) #| Out-Null
+            $data = $WordDocument.InsertList($List)
         }
     }
-    Write-Verbose "AddList - Name: $($data.GetType().Name) - BaseType: $($data.GetType().BaseType)"
+    Write-Verbose "AddWordList - Return Type Name: $($data.GetType().Name) - BaseType: $($data.GetType().BaseType)"
     if ($supress -eq $false) {
         return $data
     } else {
