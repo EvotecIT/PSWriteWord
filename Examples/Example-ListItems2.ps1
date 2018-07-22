300139bdedd71f3ce60a032bac0a353567bd1b22
PrzemyslawKlys <pklys@evotec.pl>
2018-07-22 17:32:10 +0200

    Example update

--- Examples/Example-ListItems2.ps1
+++ Examples/Example-ListItems2.ps1
@@ -16,13 +16,13 @@ $ListOfItems += $InvoiceEntry2
 
 $WordDocument = New-WordDocument $FilePath
 
-#Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True -Verbose
-Add-WordList -WordDocument $WordDocument -ListType Bulleted -ListData $ListOfItems -Supress $false -Verbose
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True -Verbose
+Add-WordList -WordDocument $WordDocument -ListType Bulleted -ListData $ListOfItems -Supress $true -Verbose
 
-#Add-WordSection -WordDocument $WordDocument -PageBreak -Supress $true
+Add-WordSection -WordDocument $WordDocument -PageBreak -Supress $true
 
-#Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be numbered list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
-#Add-WordList -WordDocument $WordDocument -ListType Numbered -ListData $ListOfItems -Supress $true
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be numbered list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
+Add-WordList -WordDocument $WordDocument -ListType Numbered -ListData $InvoiceEntry1 -Supress $true -Verbose
 
 Save-WordDocument $WordDocument -Language 'en-US' -Supress $true
 Invoke-Item $FilePath
