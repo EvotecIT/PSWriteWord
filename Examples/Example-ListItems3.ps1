d2f071a7ea65faaf7bfbe0e5d64cc880f85d7e9d
PrzemyslawKlys <pklys@evotec.pl>
2018-07-22 17:35:27 +0200

    Exampel updates

--- Examples/Example-ListItems3.ps1
+++ Examples/Example-ListItems3.ps1
@@ -15,13 +15,13 @@ $myitems1 = @(
 
 $WordDocument = New-WordDocument $FilePath
 
-#Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
 Add-WordList -WordDocument $WordDocument -ListType Bulleted -ListData $myitems -Supress $false -Verbose
 
-#Add-WordSection -WordDocument $WordDocument -PageBreak -Supress $true
+Add-WordSection -WordDocument $WordDocument -PageBreak -Supress $true
 
-#Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be numbered list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
-#Add-WordList -WordDocument $WordDocument -ListType Numbered -ListData $ListOfItems -Supress $true
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be numbered list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
+Add-WordList -WordDocument $WordDocument -ListType Numbered -ListData $myitems1 -Supress $true
 
 Save-WordDocument $WordDocument -Language 'en-US' -Supress $true
 Invoke-Item $FilePath
