fcab7bd7bf36de29aaf4b7650c9f0260b50b31d3
PrzemyslawKlys <pklys@evotec.pl>
2018-07-26 18:54:56 +0200

    Updates

--- Enums/TableOfContentsSwitches.ps1
+++ Enums/TableOfContentsSwitches.ps1
@@ -1,19 +1,22 @@
-enum TableOfContentsSwitches {
-    None = 0 -shl 0
-    A = 1 -shl 0
-    B = 1 -shl 1
-    C = 1 -shl 2
-    D = 1 -shl 3
-    F = 1 -shl 4
-    H = 1 -shl 5
-    L = 1 -shl 6
-    N = 1 -shl 7
-    O = 1 -shl 8
-    P = 1 -shl 9
-    S = 1 -shl 10
-    T = 1 -shl 11
-    U = 1 -shl 12
-    W = 1 -shl 13
-    X = 1 -shl 14
-    Z = 1 -shl 15
+Add-Type -TypeDefinition @"
+public enum TableOfContentsSwitches
+{
+  None = 0 << 0,
+  A = 1 << 0,
+  B = 1 << 1,
+  C = 1 << 2,
+  D = 1 << 3,
+  F = 1 << 4,
+  H = 1 << 5,
+  L = 1 << 6,
+  N = 1 << 7,
+  O = 1 << 8,
+  P = 1 << 9,
+  S = 1 << 10,
+  T = 1 << 11,
+  U = 1 << 12,
+  W = 1 << 13,
+  X = 1 << 14,
+  Z = 1 << 15
 }
+"@
--- Tests/Add-WordTable.Tests.ps1
+++ Tests/Add-WordTable.Tests.ps1
@@ -1,5 +1,5 @@
 #Requires -Modules Pester
-Import-Module $PSScriptRoot\..\PSWriteWord.psd1 -Force
+Import-Module $PSScriptRoot\..\PSWriteWord.psd1 #-Force
 
 Describe 'Add-WordTable' {
     It 'Given 2 tables, document should have 2 tables with proper design' {
