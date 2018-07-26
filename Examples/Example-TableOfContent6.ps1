dcd94a5957a3026cd2563a6e3510b1777ce987b8
PrzemyslawKlys <pklys@evotec.pl>
2018-07-26 18:48:52 +0200

    Update

--- Examples/Example-TableOfContent6.ps1
+++ Examples/Example-TableOfContent6.ps1
@@ -8,9 +8,6 @@ $ListsToConvertToheaders = @()
 $WordDocument = New-WordDocument -FilePath $FilePath
 Add-WordToc -WordDocument $WordDocument -Title 'Table of content' -Switches C, A -RightTabPos 15 -HeaderStyle Heading1 -Supress $True
 
-#Write-Color "lists count before ", $WordDocument.Lists.Count -Color White, Yellow
-#Write-Color "lists elements before ", $WordDocument.Lists[0].Items.Count -Color White, Yellow
-
 foreach ($Section in $ListOfHeaders) {
     $List = New-WordListItem -WordDocument $WordDocument -List $null -Text $Section -ListItemType Numbered -ContinueNumbering $true
     $List = Add-WordListItem -WordDocument $WordDocument -List $List
@@ -25,9 +22,6 @@ foreach ($List in $ListsToConvertToheaders) {
 $List = New-WordListItem -WordDocument $WordDocument -List $null -Text 'Adding one more thing' -ListItemType Numbered -ContinueNumbering $true -ListLevel 1
 $List = Add-WordListItem -WordDocument $WordDocument -List $List
 Convert-ListToHeadings -WordDocument $WordDocument -List $List -Supress $True
-
-#Write-Color "lists count after ", $WordDocument.Lists.Count -Color White, Yellow
-#Write-Color "lists elements after ", $WordDocument.Lists[0].Items.Count -Color White, Yellow
 
 Save-WordDocument $WordDocument -Language 'en-US' -Supress $True
 ### Start Word with file
