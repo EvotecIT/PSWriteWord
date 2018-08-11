29564a83f90ffbf76dfc84e29572013f90267863
PrzemyslawKlys <pklys@evotec.pl>
2018-08-11 18:58:04 +0200

    Update

--- Private/PSArray.ps1
+++ Private/PSArray.ps1
@@ -19,9 +19,62 @@ function Add-ToArray {
     )
     Write-Verbose "Add-ToArray - Element: $Element"
     $List.Add($Element) > $null
-    #return , $List
 }
+function Add-ToArrayAdvanced {
+    [CmdletBinding()]
+    param(
+        [System.Collections.ArrayList] $List,
+        [Object] $Element,
+        [switch] $SkipNull,
+        [switch] $RequireUnique,
+        [switch] $FullComparison
+    )
+    if ($SkipNull -and $Element -eq $null) {
+        Write-Verbose "Add-ToArrayAdvanced - SkipNull used"
+        return
+    }
+    if ($RequireUnique) {
+        if ($FullComparison) {
+            foreach ($ListElement in $List) {
+                if ($ListElement -eq $Element) {
+                    $TypeLeft = Get-ObjectType -Object $ListElement
+                    $TypeRight = Get-ObjectType -Object $Element
+                    if ($TypeLeft.ObjectTypeName -eq $TypeRight.ObjectTypeName) {
+                        Write-Verbose "Add-ToArrayAdvanced - RequireUnique with full comparison used"
+                        return
+                    }
+                }
+            }
+        } else {
+            if ($List -contains $Element) {
+                Write-Verbose "Add-ToArrayAdvanced - RequireUnique on name used"
+                return
+            }
+        }
+    }
+    Write-Verbose "Add-ToArrayAdvanced - Adding ELEMENT: $Element"
+    $List.Add($Element) > $null
+}
+
+function Show-Array {
+    [CmdletBinding()]
+    param(
+        [System.Collections.ArrayList] $List,
+        [switch] $WithType
+    )
+    foreach ($Element in $List) {
+        $Type = Get-ObjectType -Object $Element
+        if ($WithType) {
+            Write-Output "$Element (Type: $($Type.ObjectTypeName))"
+        } else {
+            Write-Output $Element
+        }
+    }
+}
+
+
 function Remove-FromArray {
+    [CmdletBinding()]
     param(
         [System.Collections.ArrayList] $List,
         [Object] $Element,
