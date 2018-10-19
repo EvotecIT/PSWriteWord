065be5fb54a85cbde757d2fd168120153ef22378
PrzemyslawKlys <pklys@evotec.pl>
2018-10-19 12:52:59 +0200

    Small example for Example-ListItems7

---
+++ Examples/Example-ListItems7.ps1
@@ -0,0 +1,22 @@
+Import-Module PSWriteWord -Force
+
+$FilePath = "$Env:USERPROFILE\Desktop\PSWriteWord-Example-ListItems1.docx"
+$ListOfItemsNotArray1 = 'Test1'
+$ListOfItemsNotArray2 = $false
+$ListOfItemsNotArray3 = $false, $true
+
+$OverrideLevels = @(0, 1, 2, 1, 3)
+$OverrideLevelsPartially = @(0, 3)
+
+$WordDocument = New-WordDocument $FilePath
+
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
+Add-WordList -WordDocument $WordDocument -ListType Bulleted -ListData $ListOfItemsNotArray1 -Supress $True #-Verbose
+
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
+Add-WordList -WordDocument $WordDocument -ListType Bulleted -ListData $ListOfItemsNotArray2 -Supress $True #-Verbose
+
+Add-WordText -WordDocument $WordDocument -Text 'This is text after which will be bulleted list' -FontSize 15 -UnderlineStyle singleLine -HeadingType Heading2 -Supress $True
+Add-WordList -WordDocument $WordDocument -ListType Bulleted -ListData $ListOfItemsNotArray3 -Supress $True #-Verbose
+
+Save-WordDocument $WordDocument -Language 'en-US' -Supress $true -OpenDocument
