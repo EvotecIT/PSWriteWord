79a1345f29d0dff0e80a93e5cfd603ca95d18778
PrzemyslawKlys <pklys@evotec.pl>
2018-11-29 18:54:35 +0100

    Added Merge-WordDocument

--- Public/PSWordMain.ps1
+++ Public/PSWordMain.ps1
@@ -32,6 +32,21 @@ function Get-WordDocument {
     return $WordDocument
 }
 
+function Merge-WordDocument {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipelineByPropertyName, ValueFromPipeline)][alias('Path')][string] $FilePath1,
+        [alias('Append')][string] $FilePath2,
+        [string] $FileOutput
+    )
+    $WordDocument1 = Get-WordDocument -FilePath $FilePath1
+    $WordDocument2 = Get-WordDocument -FilePath $FilePath2
+
+    $WordDocument1.InsertDocument($WordDocument2, $true)
+    $WordDocument1.SaveAs($FileOutput)
+
+}
+
 function Save-WordDocument {
     [CmdletBinding()]
     param (
