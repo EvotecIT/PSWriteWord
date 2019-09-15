6c56868f06e40c3e1714443d9eafeec84bbad9c7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-15 19:52:49 +0200

    Update

---
+++ Examples/Example-ModifyImageInHeader.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module PSWriteWord
+
+$FilePath = "$PSScriptRoot\Templates\WordTemplate-ImageInHeader.docx"
+$FilePathNew = "$PSScriptRoot\Output\WordTemplate-ImageInHeaderReplaced.docx"
+
+$ReplaceImage = 'C:\Support\GitHub\PSWriteWord\Examples\Images\Logo-FakeCompany.png'
+
+$WordDocument = Get-WordDocument -FilePath $FilePath
+
+$HeaderFirst = Get-WordHeader -WordDocument $WordDocument -Type Odd
+$HeaderFirst.Paragraphs[0]
+
+Set-WordPicture -WordDocument $WordDocument -Paragraph $HeaderFirst.Paragraphs[0] -ImagePath $ReplaceImage
+
+Save-WordDocument -WordDocument $WordDocument -OpenDocument -FilePath $FilePathNew
