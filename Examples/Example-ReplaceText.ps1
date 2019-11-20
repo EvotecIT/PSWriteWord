9f1d0a287405b3ce75fb8f2357ad07c396d506ac
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-20 22:30:37 +0100

    Replace Sample

---
+++ Examples/Example-ReplaceText.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module PSWriteWord -Force
+
+$FilePath = "$PSScriptRoot\Templates\CV-Template.docx"
+$FilePathOutput = "$PSScriptRoot\Output\CV-ReplacedText.docx"
+
+$WordDocument = Get-WordDocument -FilePath $FilePath
+
+foreach ($Paragraph in $WordDocument.Paragraphs) {
+    $Paragraph.ReplaceText('image','picture')
+}
+
+Save-WordDocument -WordDocument $WordDocument -FilePath $FilePathOutput -OpenDocument -Supress $true
