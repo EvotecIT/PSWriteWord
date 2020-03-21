3bc4524cdf5fd22d0a83b75359a1ea77bda88bff
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-03-21 11:53:09 +0100

    Update

--- Examples/Example-Bookmarks/Example-BookmarksByText.ps1
+++ Examples/Example-Bookmarks/Example-BookmarksByText.ps1
@@ -6,11 +6,15 @@ $FilePathTemplate = "$PSScriptRoot\Templates\CV-WithTextReplacement.docx"
 $WordDocument = Get-WordDocument -FilePath $FilePathTemplate
 
 foreach ($Paragraph in $WordDocument.Paragraphs) {
-    $Paragraph.ReplaceText('JOB TITLE', 'CEO of EVOTEC')
+    # You can display text as seen by PSWriteWord
+    $Paragraph.Text
+    # Case sensitive
+    $Paragraph.ReplaceText('Job Title', 'CEO of EVOTEC')
     $Paragraph.ReplaceText('School Name 1st', 'Oh my school')
     $Paragraph.ReplaceText('School Name 2nd', 'Oh my school 2')
-    $Paragraph.ReplaceText('Name Here', 'Przemysław Kłys')
+    # While word shows NAME HERE, actually it's Name HERE
+    $Paragraph.ReplaceText('Name HERE', 'Przemysław Kłys')
     $Paragraph.ReplaceText('PHONE_NUMBER', '+48 500 500 500')
 }
 
-Save-WordDocument -WordDocument $WordDocument -FilePath "$PSSCriptRoot\Output\CV-WithReplacedText.docx" -OpenDocument
+Save-WordDocument -WordDocument $WordDocument -FilePath "$PSSCriptRoot\Output\CV-WithReplacedText.docx" -OpenDocument -Supress $true
